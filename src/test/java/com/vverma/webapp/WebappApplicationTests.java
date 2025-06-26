ca436971f8bc24a5fa064b823d2b76723a93cf56
Vartika Verma <vartikav05@gmail.com>
2025-06-26 19:52:05 +0530

    Final clean-up: remove unused files and ensure project is ready

---
+++ src/test/java/com/vverma/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vverma.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
